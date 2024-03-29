04e18f9065edebb5fa5df18c1598b9f00ba17cbd
James Elliott <james-d-elliott@users.noreply.github.com>
2024-03-29 11:07:35 +1100

    feat(oidc): add most standard claims
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- internal/oidc/claims_test.go
+++ internal/oidc/claims_test.go
@@ -76,10 +76,104 @@ func TestGrantClaimsUserInfo(t *testing.T) {
 }
 
 type testDetailer struct {
-	username string
-	groups   []string
-	name     string
-	emails   []string
+	username  string
+	groups    []string
+	name      string
+	emails    []string
+	given     string
+	family    string
+	middle    string
+	nickname  string
+	profile   string
+	picture   string
+	website   string
+	gender    string
+	birthdate string
+	info      string
+	locale    string
+	number    string
+	extension string
+	address   string
+	locality  string
+	region    string
+	postcode  string
+	country   string
+}
+
+func (t testDetailer) GetGivenName() (given string) {
+	return t.given
+}
+
+func (t testDetailer) GetFamilyName() (family string) {
+	return t.family
+}
+
+func (t testDetailer) GetMiddleName() (middle string) {
+	return t.middle
+}
+
+func (t testDetailer) GetNickname() (nickname string) {
+	return t.nickname
+}
+
+func (t testDetailer) GetProfile() (profile string) {
+	return t.profile
+}
+
+func (t testDetailer) GetPicture() (picture string) {
+	return t.picture
+}
+
+func (t testDetailer) GetWebsite() (website string) {
+	return t.website
+}
+
+func (t testDetailer) GetGender() (gender string) {
+	return t.gender
+}
+
+func (t testDetailer) GetBirthdate() (birthdate string) {
+	return t.birthdate
+}
+
+func (t testDetailer) GetZoneInfo() (info string) {
+	return t.info
+}
+
+func (t testDetailer) GetLocale() (locale string) {
+	return t.locale
+}
+
+func (t testDetailer) GetPhoneNumber() (number string) {
+	return t.number
+}
+
+func (t testDetailer) GetPhoneExtension() (extension string) {
+	return t.extension
+}
+
+func (t testDetailer) GetOpenIDConnectPhoneNumber() (number string) {
+	return t.number
+}
+
+func (t testDetailer) GetStreetAddress() (address string) {
+	return t.address
+}
+
+func (t testDetailer) GetLocality() (locality string) {
+	return t.locality
+}
+
+func (t testDetailer) GetRegion() (region string) {
+	return t.region
+}
+
+func (t testDetailer) GetPostalCode() (postcode string) {
+	return t.postcode
+}
+
+func (t testDetailer) GetCountry() (country string) {
+	return t.country
 }
 
 func (t testDetailer) GetUsername() (username string) {
